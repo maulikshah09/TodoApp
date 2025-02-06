05392e74d7235c1552f5b4cce496838d35cb5bf2
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-06 14:26:45 +0530

    Maulik. -> Init Setup

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Maulik Shah on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
